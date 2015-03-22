19ba724b1945d185570000b555e7ec89a7783ca9
Liam Marshall <archimedespi3141@gmail.com>
2015-03-22 14:50:53 -0500

    Start coding joystick support

--- main.cpp
+++ main.cpp
@@ -8,17 +8,30 @@ int main() {
     std::cout << "Stuff which is buit in:\n";
     #ifdef USE_JOYSTICK
     std::cout << "Joystick support built in.\n";
-    #endif
-
     
+    sf::Joystick::Identification joystickId = sf::Joystick::getIdentification(0);
+    std::cout << "Vendor ID: " << joystickId.vendorId << std::endl << "Product ID: " << joystickId.productId << std::endl;
+    #endif
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
+                std::cout << "Quit from <WindowManager>" << std::endl;
                 window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                std::cout << "Quit from <ESC>" << std::endl;
+            }
         }
+
+        #ifdef USE_JOYSTICK
+        if (sf::Joystick::isConnected(0)) {
+            unsigned int buttonCount = sf::Joystick::getButtonCount(0);
+        }
+        #endif
 
         window.clear();
         window.display();
