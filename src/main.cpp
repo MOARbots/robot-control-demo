ddacbe5791ce601493f2c2a7b5f6705b3738c485
Liam Marshall <archimedespi3141@gmail.com>
2015-03-26 11:32:06 -0500

    Try to open SDL

--- src/main.cpp
+++ src/main.cpp
@@ -21,5 +21,10 @@
 #include <SDL.h>
 
 int main(int argc, char** argv) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL init error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Quit();
 	return 0;
 }
